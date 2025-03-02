1020d6f28b2e81021cce0647dd7d92273615c4f1
Akane07 <Oreki05@mail.ru>
2025-03-03 01:04:03 +0300

    сверстал самый противный блок. почему то так быстро

--- src/components/homePage/SecondBlock.vue
+++ src/components/homePage/SecondBlock.vue
@@ -3,7 +3,23 @@
     <div class="content">
         <div class="text">Почему выберают<br> нас?</div>
         <div class="left">
-
+            <div class="interface">
+                <div class="interface-content">
+                    <div class="interface-text">
+                        <span class="one">1</span>
+                        <span class="one-title">Простой интерфейс</span>
+                    </div>
+                    <div class="interface-context">
+                        <span>Мы сделали наш сайт интуитивно понятным, чтобы вы могли легко начать</span>
+                    </div>
+                </div>
+                <div class="interface-bottom">
+                    <div class="inteface-bottom-text"><span>Отслеживать свои финансы без лишних усилий.</span></div>
+                </div>
+            </div>
+            <div class="analysis">
+                <div class="analysis-top"></div>
+            </div>
         </div>
         <div class="center">
             <span class="five">5</span>
@@ -19,7 +35,12 @@
             </span>
         </div>
         <div class="right">
-
+            <div class="target">
+                <div class="target-bottom"></div>
+            </div>
+            <div class="safety">
+                <div class="safety-top"></div>
+            </div>
         </div>
     </div>
 </div>
@@ -44,6 +65,7 @@
     padding-bottom: 50px;
     display: flex;
     justify-content: space-between;
+    gap: 32px;
 }
 .text{
     color: #000000;
@@ -54,6 +76,50 @@
     left: 15%;
     right: 15%;
     white-space: pre-line;
+}
+.interface-content{
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: space-around;
+    z-index: 1;
+}
+.interface-text{
+    display: flex;
+    align-items: center;
+    gap: 18px;
+    padding-left: 51px;
+}
+.interface-context{
+    z-index: 1;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 28.8px;
+    letter-spacing: 0%;
+    color: #FFFFFF;
+    padding-left: 10px;
+}
+.inteface-bottom-text{
+    z-index: 1;
+    position: relative;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 28.8px;
+    letter-spacing: 0%;
+    color: #FFFFFF;
+    padding-left: 10px;
+    text-align: center;
+}
+.one{
+    color: #ffffff;
+    font-size: 96px;
+    font-weight: 600;
+    height: auto;
+}
+.one-title{
+    color: #b5182d;
+    font-size: 40px;
+    text-align: center;
 }
 .five {
     color: #ffffff;
@@ -94,6 +160,141 @@
     color: #FFFFFF;
     white-space: pre;
 }
+.left {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 45%;
+    height: auto;
+}
+.interface {
+    width: 100%;
+    background: #1E1E27;
+    height: 37%;
+    border-top-left-radius: 85px;
+    border-top-right-radius: 85px;
+    border-bottom-right-radius: 85px;
+    position: relative;
+}
+.interface-bottom{
+    position: absolute;
+    background: #1E1E27;
+    width: 40%;
+    height: 66%;
+    bottom: -65%;
+    border-bottom-left-radius: 85px;
+    border-bottom-right-radius: 85px;
+}
+.interface-bottom::before{
+    content: "";
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    background: transparent;
+    border-radius: 50%;
+    top: 0px;
+    right: -90px;
+    box-shadow: -40px -40px 0 #1E1E27;
+    z-index: 0;
+}
+.analysis {
+    width: 100%;
+    background: #1E1E27;
+    height: 37%;
+    border-bottom-left-radius: 85px;
+    border-bottom-right-radius: 85px;
+    border-top-left-radius: 85px;
+    position: relative;
+}
+.analysis-top{
+    position: absolute;
+    background: #1E1E27;
+    width: 40%;
+    height: 66%;
+    top: -65%;
+    right: 0px;
+    border-top-left-radius: 85px;
+    border-top-right-radius: 85px;
+}
+.analysis-top::after{
+    content: "";
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    background: transparent;
+    border-radius: 50%;
+    bottom: 0px;
+    left: -90px;
+    box-shadow: 40px 40px 0 #1E1E27;}
+.right {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 45%;
+    height: auto;
+}
+.target {
+    width: 100%;
+    background: #1E1E27;
+    height: 37%;
+    border-top-left-radius: 85px;
+    border-top-right-radius: 85px;
+    border-bottom-left-radius: 85px;
+    position: relative;
+}
+.target-bottom{
+    position: absolute;
+    background: #1E1E27;
+    width: 40%;
+    height: 66%;
+    bottom: -65%;
+    right: 0px;
+    border-bottom-left-radius: 85px;
+    border-bottom-right-radius: 85px;
+}
+.target-bottom::before{
+    content: "";
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    background: transparent;
+    border-radius: 50%;
+    top: 0px;
+    left: -90px;
+    box-shadow: 40px -40px 0 #1E1E27;
+}
+.safety {
+    width: 100%;
+    background: #1E1E27;
+    height: 37%;
+    border-bottom-left-radius: 85px;
+    border-bottom-right-radius: 85px;
+    border-top-right-radius: 85px;
+    position: relative;
+}
+.safety-top{
+    position: absolute;
+    background: #1E1E27;
+    width: 40%;
+    height: 66%;
+    top: -65%;
+    left: 0px;
+    border-top-left-radius: 85px;
+    border-top-right-radius: 85px;
+}
+.safety-top::after{
+    content: "";
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    background: transparent;
+    border-radius: 50%;
+    bottom: 0px;
+    right: -89px;
+    box-shadow: -40px 40px 0 #1E1E27;}
+
 </style>
 
 <script setup>
